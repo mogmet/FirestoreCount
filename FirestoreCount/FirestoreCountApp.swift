b962f55e06a4197836fab8804dbf55230e936a82
mogmet <mogmet@gmail.com>
2022-10-12 07:17:08 +0900

    Initial Commit

---
+++ FirestoreCount/FirestoreCountApp.swift
@@ -0,0 +1,17 @@
+//
+//  FirestoreCountApp.swift
+//  FirestoreCount
+//
+//  Created by mogmet on 2022/10/12.
+//
+
+import SwiftUI
+
+@main
+struct FirestoreCountApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
