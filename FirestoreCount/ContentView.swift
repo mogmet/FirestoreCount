6b4dc075560c3181a6447efe0054044225758643
mogmet <mogmet@gmail.com>
2022-10-12 09:15:08 +0900

    feature: count

--- FirestoreCount/ContentView.swift
+++ FirestoreCount/ContentView.swift
@@ -6,16 +6,30 @@
 //
 
 import SwiftUI
+import FirebaseFirestore
 
 struct ContentView: View {
+    @State var countText: String = "none"
     var body: some View {
         VStack {
+            Text(countText)
+            Button {
+                Task {
+                    await loadCount()
+                }
+            } label: {
+                Text("Load count")
+            }
             Image(systemName: "globe")
                 .imageScale(.large)
                 .foregroundColor(.accentColor)
             Text("Hello, world!")
         }
         .padding()
+    }
+    func loadCount() async {
+        let aggregateSnapshot = try! await Firestore.firestore().collection("users").count.getAggregation(source: .server)
+        countText = aggregateSnapshot.count.stringValue
     }
 }
 
--- FirestoreCount/FirestoreCountApp.swift
+++ FirestoreCount/FirestoreCountApp.swift
@@ -6,9 +6,20 @@
 //
 
 import SwiftUI
+import Firebase
+import FirebaseFirestore
 
 @main
 struct FirestoreCountApp: App {
+    init() {
+        FirebaseApp.configure()
+        let settings = Firestore.firestore().settings
+        settings.host = "localhost:8080"
+        settings.isPersistenceEnabled = false
+        settings.isSSLEnabled = false
+        Firestore.firestore().settings = settings
+
+    }
     var body: some Scene {
         WindowGroup {
             ContentView()
